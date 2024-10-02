085425fb7ba74344a75dc5c17c54b54a338ee97e
L1MBeR1 <fishpaol@gmail.com>
2024-10-02 22:19:41 +0800

    check

--- src/main.ts
+++ src/main.ts
@@ -13,26 +13,26 @@ async function bootstrap() {
 	app.use(cookieParser());
 
 	app.enableCors({
-		origin: true,
+		origin: [process.env.FRONT_URL],
 		credentials: true,
 		exposedHeaders: ['set-cookie'],
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
 	});
 
-	app.use((req, res, next) => {
-		if (req.method === 'OPTIONS') {
-			res.header('Access-Control-Allow-Origin', process.env.FRONT_URL);
-			res.header(
-				'Access-Control-Allow-Methods',
-				'GET, POST, PUT, DELETE, OPTIONS',
-			);
-			res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-			res.header('Access-Control-Allow-Credentials', 'true');
-			return res.sendStatus(200);
-		}
-		next();
-	});
+	// app.use((req, res, next) => {
+	// 	if (req.method === 'OPTIONS') {
+	// 		res.header('Access-Control-Allow-Origin', process.env.FRONT_URL);
+	// 		res.header(
+	// 			'Access-Control-Allow-Methods',
+	// 			'GET, POST, PUT, DELETE, OPTIONS',
+	// 		);
+	// 		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	// 		res.header('Access-Control-Allow-Credentials', 'true');
+	// 		return res.sendStatus(200);
+	// 	}
+	// 	next();
+	// });
 	await app.listen(4200);
 }
 
